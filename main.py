bd6e003258fa7d8bc9fdcf6f3ec40a4726ea2672
NiumXp <43611165+NiumXp@users.noreply.github.com>
2020-08-20 21:18:08 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 from flask import Flask, render_template, url_for
 
-app = Flask(__name__)
+def make_app():
+    app = Flask(__name__)
 
-@app.route('/')
-def home():
-    nium_avatar = url_for('static', filename='img/nium-avatar.gif')
-    marjorie_avatar = url_for('static', filename='img/marjorie-avatar.png')
+    @app.route('/')
+    def home():
+        nium_avatar = url_for('static', filename='img/nium-avatar.gif')
+        marjorie_avatar = url_for('static', filename='img/marjorie-avatar.png')
 
-    return render_template("main.html", nium_avatar=nium_avatar, marjorie_avatar=marjorie_avatar)
+        return render_template("main.html", nium_avatar=nium_avatar, marjorie_avatar=marjorie_avatar)
 
-if __name__ == '__main__':
-    app.run(debug=True)
+    return app
