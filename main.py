259ad0c75460c5d4267cc91bf192896020e37922
NiumXp <43611165+NiumXp@users.noreply.github.com>
2020-08-20 21:03:01 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template, url_for
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    nium_avatar = url_for('static', filename='img/nium-avatar.gif')
+    marjorie_avatar = url_for('static', filename='img/marjorie-avatar.png')
+
+    return render_template("main.html", nium_avatar=nium_avatar, marjorie_avatar=marjorie_avatar)
+
+app.run()
